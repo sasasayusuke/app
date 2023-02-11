09df43e5742155eff71bf6d1294e13cc6f5bb509
sasasa <sasasayusuke@gmail.com>
2023-02-12 04:28:20 +0900

    create

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,9 +5,45 @@ from flaskr import common, setting
 MASTER_DATABASE = "master.db"
 TRUN_DATABASE = "trun.db"
 
+
+def createImages():
+    con = sqlite3.connect(MASTER_DATABASE)
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS
+            images (
+                id integer primary key autoincrement,
+                path,
+                `like` int,
+                score int,
+                message text
+            )
+    """)
+    con.commit()
+    con.close
+
+def createPlayLog():
+    con = sqlite3.connect(TRUN_DATABASE)
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS
+            playLog (
+                session_id int not null,
+                player_count int,
+                winner int,
+                loser int,
+                winner_score int,
+                loser_score int
+            )
+    """)
+    con.commit()
+    con.close
+
 def insertImages():
     con = sqlite3.connect(MASTER_DATABASE)
-    db_paths = con.execute("SELECT path FROM images").fetchall()
+    db_paths = con.execute("""
+        SELECT
+            path
+        FROM images
+    """).fetchall()
     paths = [item[0] for item in db_paths]
 
     extensions = ["png", "jpg", "jpeg", "webp", "gif", "jfif"]
@@ -19,7 +55,18 @@ def insertImages():
     for file in allFIle:
         path = file.split(setting.APPNAME)[1]
         if (path not in paths):
-            con.execute(f'INSERT INTO images(path, score, like) VALUES("{path}", "99", "0")')
+            con.execute(f"""
+                INSERT INTO
+                    images(
+                        path,
+                        score,
+                        like
+                    ) VALUES (
+                        "{path}",
+                        "99",
+                        "0"
+                    )
+            """)
     con.commit()
     con.close
 
@@ -27,7 +74,7 @@ def insertPlayLogs(playLogs):
 
     con = sqlite3.connect(TRUN_DATABASE)
     for log in playLogs:
-        con.execute(f'''
+        con.execute(f"""
             INSERT INTO
                 playLog(
                     id,
@@ -36,14 +83,14 @@ def insertPlayLogs(playLogs):
                     loser,
                     winner_score,
                     loser_score
-                ) VALUES(
+                ) VALUES (
                     "{log['session']}",
                     "{log['count']}",
                     "{log['winner']}",
                     "{log['loser']}",
                     "{log['winner_score']}",
                     "{log['loser_score']}")
-        ''')
+        """)
     con.commit()
     con.close
 
